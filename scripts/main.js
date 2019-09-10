914cfbf5f1c93f0b7bbddf1c5a9727952800223b
Losalini <C.Rokocakau@gmail.com>
2019-09-10 20:25:20 +1200

    Make new variable for playRound function, print value of new variable on console, remove newPlayerScore and newComputer score variable, indent comments on code

--- scripts/main.js
+++ scripts/main.js
@@ -18,7 +18,8 @@ let numberOfGameRounds;
 
 /*Function declarations*/
        
-//A function that will randomly return either 'Rock', 'Paper' or 'Scissors'. Using this function in the game makes the computer play
+//A function that will randomly return either 'Rock', 'Paper' or 'Scissors'. 
+//Using this function in the game makes the computer play
 function computerPlay () {
            
      possibleComputerChoices = ["rock", "paper", "scissors"];
@@ -35,30 +36,32 @@ function computerPlay () {
 function playRound (playerSelection, computerSelection) {
 
      /*If player choice beats computer choice, increment player's score and return winning message on console.
-     Or if player choice does not beat computer choice, increment computer's score and return losing message on console.
+    Or if player choice does not beat computer choice, increment computer's score and return losing message on console.
     Otherwise, do not increment player's score and computer's score and return a tie message on console*/
 
-    if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'rock')) {
+    if ((playerSelection === 'rock' && computerSelection === 'scissors') || 
+        (playerSelection === 'scissors' && computerSelection === 'paper') || 
+        (playerSelection === 'paper' && computerSelection === 'rock')) {
    
-         //make playerScore increment (the increment equates to 2) and computerScore is still zero
-         newPlayerScore = ++playerScore;
-         newComputerScore = computerScore;
+         //make playerScore increment and computerScore is still zero
+         playerScore++;
 
-         return ("You win! Your score: " + newPlayerScore + " and Computer's score: " + newComputerScore) ;
+         return ("You win! Your score: " + playerScore + " and Computer's score: " + computerScore) ;
 
-    } else if ((playerSelection === 'scissors' && computerSelection === 'rock') || (playerSelection === 'paper' && computerSelection === 'scissors') || (playerSelection === 'rock' && computerSelection === 'paper')) {
+    } else if ((playerSelection === 'scissors' && computerSelection === 'rock') ||
+               (playerSelection === 'paper' && computerSelection === 'scissors') || 
+               (playerSelection === 'rock' && computerSelection === 'paper')) {
 
-         newComputerScore = ++computerScore;
-         newPlayerScore = playerScore;
+         computerScore++;
        
-         return ("You lose! " + computerSelection + " beats " + playerSelection + ". Your score: " + newPlayerScore + " and Computer's score: " + newComputerScore);
+         return ("You lose! " + computerSelection + " beats " + playerSelection + ". Your score: " + playerScore + " and Computer's score: " + computerScore);
 
     } else {
 
-         newComputerScore = computerScore;
-         newPlayerScore = playerScore;
+         computerScore;
+         playerScore;
 
-         return ("We're tied! Your score: " + newPlayerScore + " and Computer's score: " + newComputerScore);
+         return ("We're tied! Your score: " + playerScore + " and Computer's score: " + computerScore);
 
     }
 
@@ -79,18 +82,19 @@ for (let gameRounds = 1; gameRounds <= 5; gameRounds++) {
        
      playerSelection = playerSelection.toLowerCase();         //makes the input (a string) to lower case and store in the playerSelection variable
 
-     //If the value in the playerSelection variable is either 'rock or 'paper' or 'scissors', then execute the following code in order from top to bottom
+     //If the value in the playerSelection variable is either 'rock or 'paper' or 'scissors', 
+     //then execute the following code in order from top to bottom
      if ((playerSelection === 'rock') || (playerSelection === 'paper') || (playerSelection === 'scissors'))  {
                
          computerSelection = computerPlay();                
 
-         playRound(playerSelection, computerSelection);        
+         let message = playRound(playerSelection, computerSelection);        
 
          console.log("My choice: " + computerSelection);      
 
-         console.log(playRound(playerSelection, computerSelection));        //prints the winning/losing/tie message and the player's and computer's score on the console 
+         console.log(message);        //prints the winning/losing/tie message and the player's and computer's score on the console 
 
-    } else {         //Otherwise, when value of playerSelection is either empty or when the string is not equal to 'rock/paper/scissors'  or when value is not a string, then execute code below
+    } else {         //Otherwise, when value of playerSelection is either empty or when the string is not equal to 'rock/paper/scissors' or when value is not a string, then execute code below
 
          //keep repeating the code below  (asking for the correct input) until the right input is being entered - not finished yet with this part of the code
          while ((playerSelection !== 'rock') || (playerSelection !== 'paper') || (playerSelection !== 'scissors')); {
